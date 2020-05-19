ab2cc5d03df1425d34e79e9b5b82bffc1583a9fe
ahmed jamal <ahjamalm@gmail.com>
2020-05-20 01:23:24 +0400

    Updates

---
+++ precache-manifest.36ddffa529d70d1be4536ba86d3d7b17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0602ec9cf5db7ea5665d3deec401022",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30937bbaa95e1bccc8b7",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f81867e0a461438ad9b6",
+    "url": "/static/js/2.4ef118ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4ef118ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30937bbaa95e1bccc8b7",
+    "url": "/static/js/main.c5575d17.chunk.js"
+  },
+  {
+    "revision": "24e8d5a84308de897e3a",
+    "url": "/static/js/runtime-main.c8f0954f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
